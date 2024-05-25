a23028ed0a4795660adb7589cb7ddd567fed0981
zmakhkha <jcaladic@gmail.com>
2024-05-25 13:59:44 +0100

    rectified : 100

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 
-DB_NAME=$MDB_NAME
-DB_USER=$MDB_USER
-DB_PASS=$MDB_PASS
-
 mysql_install_db
 service mariadb start
 
 mysql -u root -e "DROP DATABASE IF EXISTS test;"
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME; GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
-mysql -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DB_PASS');"
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MDB_NAME; GRANT ALL ON $MDB_NAME.* TO '$MDB_USER'@'%' IDENTIFIED BY '$MDB_PASS';"
+# mysql -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MDB_PASS');"
+# mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MDB_PASS}';"
 mysql -u root -e "FLUSH PRIVILEGES;"
-mysqladmin shutdown -p$DB_PASS
+mysqladmin shutdown -p$MDB_PASS
 exec mysqld --user=mysql
 
