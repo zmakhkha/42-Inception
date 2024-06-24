f04bb068cab2918455ba62413e14acd5b0275e6c
zmakhkha <jcaladic@gmail.com>
2024-06-25 00:23:44 +0100

    working on ftp needs to be tested

---
+++ srcs/requirements/bonus/ftp/tools/script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+adduser --home /var/www ${FTP_USER}
+
+echo ${FTP_USER}:${FTP_PWD} | chpasswd
+
+sed -i -r "s/#write_enable=YES/write_enable=YES/1"   /etc/vsftpd.conf
+
+service vsftpd stop
+
+/usr/sbin/vsftpd /etc/vsftpd.conf
